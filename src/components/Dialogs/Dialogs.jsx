ce4f6fca5905198601f7123af33c86db42947987
Myakotin Dmitriy <d.myakotin@mininform74.ru>
2023-01-25 13:34:43 +0500

    add html and scss style for page dialogs
    add html and scss style for page dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,7 +6,28 @@ const Dialogs = (props) => {
         <section className="dialogs">
             <div className="dialogs__container">
                 <div className="dialogs__content">
-                Dialogs
+                    <div className="dialogs__items item-dialog">
+                        <h4 className="item-dialog__title title-dialog">Диалоги</h4>
+                        <div className="item-dialog__content">
+                            <div className="item-dialog__body">
+                                <p className="item-dialog__name">Dima</p>
+                            </div>
+                            <div className="item-dialog__body">
+                                <p className="item-dialog__name">Dima</p>
+                            </div>
+                            <div className="item-dialog__body">
+                                <p className="item-dialog__name">Dima</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dialogs__messages message-item">
+                        <h4 className="message-item__title title-dialog">Чат</h4>
+                        <div className="message-item__content">
+                            <div className="message-item__body">
+                                <p>Hi</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
