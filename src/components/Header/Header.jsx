59f87c50343a6b38eea72be6a98816b4989ffd97
Myakotin Dmitriy <d.myakotin@mininform74.ru>
2023-01-25 11:09:12 +0500

    change structures project
    moved some styles from app.scss to index.scss, added image folder, and added variable.scss file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.scss';
+import logo  from '../../images/header/logo.svg'
 
 const Header = () =>{
     return(
         <header className="header">
             <div className="header__container">
                 <div className="header__content">
-                    <a href="#" className="header__logo"><img src="https://e7.pngegg.com/pngimages/41/701/png-clipart-logo-graphic-design-graphy-grapher-logo-graphy-angle-text.png" /></a>
+                    <a href="#" className="header__logo"><img src={logo}  alt='Логотип'/></a>
                     <a href="#" className="header__title">Социальная сеть</a>
                 </div>
             </div>
