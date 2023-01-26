f0484f40c5a75c3ce41adbfdb7be5c9b4de23155
Myakotin Dmitriy <d.myakotin@mininform74.ru>
2023-01-26 10:06:57 +0500

    add objects for data
    add objects for dialogs, posts and message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,20 +4,35 @@ import DialogItem from './DialogItem/DialogItem'
 import Message from './Message/Message'
 
 const Dialogs = (props) => {
+
+    let dialogsData =
+        [{id:1,name:"Dmitriy"},
+        {id:2,name:"Sergey"},
+        {id:3,name:"Katy"},
+        {id:4,name:"Andrew"}
+    ]
+
+    let messagesData=
+        [{id:1, message:"hi"},
+            {id:2, message:"efw"},
+            {id:3, message:"gtrg"},
+        ]
+
+    let dialogsElement = dialogsData.map(dialog => <DialogItem id={dialog.id} name={dialog.name} />);
+    let messagesElement = messagesData.map(message => <Message id={message.id} message={message.message} />);
+
+
     return (
         <section className="dialogs">
             <div className="dialogs__container">
                 <div className="dialogs__content">
                     <div className="dialogs__items item-dialog">
                         <h4 className="item-dialog__title title-dialog">Диалоги</h4>
-                        <DialogItem name="Dima" id="1"/>
-                        <DialogItem name="Sergey" id="2"/>
-                        <DialogItem name="Katy" id="3"/>
+                        {dialogsElement}
                     </div>
                     <div className="dialogs__messages message-item">
                         <h4 className="message-item__title title-dialog">Чат</h4>
-                        <Message message="hi" />
-                        <Message message="hi" />
+                        {messagesElement}
                     </div>
                 </div>
             </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './MyPosts.scss'
 import Post from './Post/Post'
+import DialogItem from "../../Dialogs/DialogItem/DialogItem";
 
 const MyPosts= () => {
+
+    let postData =[
+        {id:1, message:'hello', likescount:12},
+        {id:2, message:'ko', likescount:4},
+        {id:3, message:'second', likescount:11},
+    ]
+    let postsElement = postData.map(post => <Post id={post.id} message={post.message} likes={post.likescount} />);
+
     return (
       <div className="post">
         <div className="post__container">
@@ -14,8 +23,7 @@ const MyPosts= () => {
             </form>
             <div className="post__board board-post">
               <h4 className="board-post title-post">Мои посты:</h4>
-              <Post message="Hello"/>
-              <Post message="Second post"/>
+              {postsElement}
             </div>
           </div>
         </div>
