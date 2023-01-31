0ab1a7b8fc2004c5c060b616a8fe298ecd892062
Myakotin Dmitriy <d.myakotin@mininform74.ru>
2023-01-31 10:42:40 +0500

    add store, implementation oop
    add store and implementation oop

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App'
-import state, {addMessage, addPost, subscribe, updateNewMessageText, updateNewPostText} from "./redux/State";
+import store from './redux/State'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let rerenderEntireTree = (state) => {
     root.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost} updateNewPostText ={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText} />
+            <App state={state} addPost={store.addPost.bind(store)} updateNewPostText ={store.updateNewPostText.bind(store)} //bind() делает что this будет всегда store
+                 addMessage={store.addMessage.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)} />
         </React.StrictMode>);
 };
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree)
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,83 +1,82 @@
-let rerenderEntireTree = () =>{
-    console.log("Test")
-}
-
-let state ={
-    profile:{
-        postsData: [
-            {id:1, message:'hello', likescount:12},
-            {id:2, message:'ko', likescount:4},
-            {id:3, message:'second', likescount:11},
-        ],
-        newPostText: ''
+let store ={
+    _state: {
+        profile:{
+            postsData: [
+                {id:1, message:'hello', likescount:12},
+                {id:2, message:'ko', likescount:4},
+                {id:3, message:'second', likescount:11},
+            ],
+            newPostText: ''
+        },
+        dialogs:{
+            dialogsData: [
+                {id:1,name:'Dmitriy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:2,name:'Sergey',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:3,name:'Katy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:4,name:'Andrew',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:1,name:'Dmitriy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:2,name:'Sergey',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:3,name:'Katy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+            ],
+            messagesData:[
+                {id:1, message:'Hello, how are you?'},
+                {id:2, message:'efw'},
+                {id:3, message:'gtrg'},
+            ],
+            newMessageText: ''
+        },
+        navigation:{
+            friendsData: [
+                {id:1,name:'Dmitriy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:2,name:'Sergey',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:3,name:'Katy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
+                {id:4,name:'Andrew',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'}
+            ]
+        }
     },
-    dialogs:{
-        dialogsData: [
-            {id:1,name:'Dmitriy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:2,name:'Sergey',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:3,name:'Katy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:4,name:'Andrew',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:1,name:'Dmitriy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:2,name:'Sergey',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:3,name:'Katy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-        ],
-        messagesData:[
-            {id:1, message:'Hello, how are you?'},
-            {id:2, message:'efw'},
-            {id:3, message:'gtrg'},
-        ],
-        newMessageText: ''
+    getState(){
+        return this._state;
     },
-    navigation:{
-        friendsData: [
-            {id:1,name:'Dmitriy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:2,name:'Sergey',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:3,name:'Katy',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'},
-            {id:4,name:'Andrew',avatar:'https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'}
-        ]
+    _callSubscriber(){
+        console.log("State changes")
+    },
+    addPost(){
+        if (this._state.profile.newPostText){
+            let newPost = {
+                id: 1,
+                //получаем значение текста из state который сам обновляется
+                message: this._state.profile.newPostText,
+                likescount: 0
+            };
+            this._state.profile.postsData.push(newPost);
+            this._state.profile.newPostText = '';
+        }
+       this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText){
+        this._state.profile.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage () {
+        if (this._state.dialogs.newMessageText){
+            let newMessage = {
+                id: 1,
+                //получаем значение текста из state который обновляется при вводе любого символа в чате
+                message: this._state.dialogs.newMessageText
+            };
+            this._state.dialogs.messagesData.push(newMessage);
+            this._state.dialogs.newMessageText = '';
+        }
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newText){
+        this._state.dialogs.newMessageText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer){
+        //Перерисовка страницы
+        this._callSubscriber = observer; // Наблюдатель паттерн
     }
 }
 
-export const addPost = () =>{
-    if (state.profile.newPostText){
-        let newPost = {
-            id: 1,
-            //получаем значение текста из state который сам обновляется
-            message: state.profile.newPostText,
-            likescount: 0
-        };
-        state.profile.postsData.push(newPost);
-        state.profile.newPostText = '';
-    }
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) =>{
-    state.profile.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const addMessage = () =>{
-    if (state.dialogs.newMessageText){
-        let newMessage = {
-            id: 1,
-            //получаем значение текста из state который сам обновляется
-            message: state.dialogs.newMessageText
-        };
-        state.dialogs.messagesData.push(newMessage);
-        state.dialogs.newMessageText = '';
-    }
-    rerenderEntireTree(state);
-}
-
-export const updateNewMessageText = (newText) =>{
-    state.dialogs.newMessageText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) =>{
-    //Перерисовка страницы
-    rerenderEntireTree = observer; // Наблюдатель паттерн
-}
-
-export default state;
+export default store;
