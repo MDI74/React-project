a5d995a03df7b9a4237c0dba99bb11e31b22bdbc
Myakotin Dmitriy <d.myakotin@mininform74.ru>
2023-02-09 09:06:07 +0500

    ajax requests moved from users component to usersContainer
    ajax requests moved from users component to usersContainer

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,86 +1,66 @@
 import React from "react";
 import "./Users.scss";
 import User from "./User/User";
-import axios from "axios";
 
-class Users extends React.Component {
+const Users = (props) => {
 
-    //Вызывается после рендеринга компонента
-    componentDidMount() {
-        //Подгружаем данные из api
-        if (this.props.users.length === 0) {
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            })
-        }
+    let usersElement = props.users.map(user => <User addFriend={props.addFriend}
+                                                          delFriend={props.delFriend}
+                                                          key={user.id} id={user.id} follow={user.followed}
+                                                          name={user.name}
+                                                          status={user.status}
+        // city={user.location.city} country={user.location.country}
+                                                          photo={user.photos.small}/>);
+
+    //Считает количество страниц
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+
     }
+    //Создаем карусель массива страниц
+    let startPage = (props.currentPage - 5) < 0 ? 0 : props.currentPage - 5;
+    let endPage = props.currentPage + 4;
+    let currentPages = pages.slice(startPage, endPage);
 
-    onPageChanged = (page) => {
-        this.props.setCurrentPage(page);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        })
-    }
+    //Создаем элемент со всеми страницами
+    let pagesElement = currentPages.map(page => <span
+        className={props.currentPage === page ? "pagination-user__page active" : "pagination-user__page"}
+        onClick={(e) => props.onPageChanged(page)}>{page}</span>)
 
-    render() {
-        let usersElement = this.props.users.map(user => <User addFriend={this.props.addFriend}
-                                                              delFriend={this.props.delFriend}
-                                                              key={user.id} id={user.id} follow={user.followed}
-                                                              name={user.name}
-                                                              status={user.status}
-            // city={user.location.city} country={user.location.country}
-                                                              photo={user.photos.small}/>);
+    //Кнопки для перелистывания страниц вперед, назад
+    let nextPage = <span className="pagination-user__page"
+                         onClick={(e) => props.onPageChanged(props.currentPage >= props.totalUsersCount ? props.currentPage : props.currentPage + 1)}> 🠆 </span>;
+    let backPage = <span className="pagination-user__page"
+                         onClick={(e) => props.onPageChanged(props.currentPage <= 1 ? props.currentPage : props.currentPage - 1)}> 🠄 </span>;
 
-        //Считает количество страниц
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+    pagesElement = [...pagesElement, nextPage];
 
-        let pages = []
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
+    pagesElement = [backPage, ...pagesElement];
 
-        }
-
-        //Создаем карусель массива страниц
-        let startPage = (this.props.currentPage - 5) < 0 ? 0 : this.props.currentPage - 5;
-        let endPage = this.props.currentPage + 4;
-        let currentPages = pages.slice(startPage, endPage);
-
-        //Создаем элемент со всеми страницами
-        let pagesElement = currentPages.map(page => <span
-            className={this.props.currentPage === page ? "pagination-user__page active" : "pagination-user__page"}
-            onClick={(e) => this.onPageChanged(page)}>{page}</span>)
-
-        let nextPage = <span className="pagination-user__page"
-                             onClick={(e) => this.onPageChanged(this.props.currentPage >= this.props.totalUsersCount ? this.props.currentPage : this.props.currentPage + 1)}> 🠆 </span>;
-        let backPage = <span className="pagination-user__page"
-                             onClick={(e) => this.onPageChanged(this.props.currentPage <= 1 ? this.props.currentPage : this.props.currentPage - 1)}> 🠄 </span>;
-
-        pagesElement = [...pagesElement, nextPage];
-
-        pagesElement = [backPage, ...pagesElement];
-        return (
-            <section className="users">
-                <div className="users__container">
-                    <div className="users__content">
-                        <div className="user__topmenu">
-                            <p className="users__title">Найти друзей</p>
+    return (
+        <section className="users">
+            <div className="users__container">
+                <div className="users__content">
+                    <div className="user__topmenu">
+                        <p className="users__title">Найти друзей</p>
+                    </div>
+                    <div className="users__item item-user">
+                        <div className="item-user__content">
+                            {usersElement}
                         </div>
-                        <div className="users__item item-user">
-                            <div className="item-user__content">
-                                {usersElement}
-                            </div>
-                        </div>
-                        <div className={"users__pagination pagination-user"}>
-                            <div className="pagination-user__content">
-                                {pagesElement}
-                            </div>
+                    </div>
+                    <div className={"users__pagination pagination-user"}>
+                        <div className="pagination-user__content">
+                            {pagesElement}
                         </div>
                     </div>
                 </div>
-            </section>
-        )
-    }
-}
+            </div>
+        </section>
+    )
 
+}
 export default Users;
--- src/components/Users/UsersСontainer.jsx
+++ src/components/Users/UsersСontainer.jsx
@@ -1,6 +1,36 @@
+import React from "react";
 import {connect} from "react-redux";
-import Users from "./Users";
 import {addFriendAC, delFriendAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC} from "../../redux/Users-reducer";
+import axios from "axios";
+import Users from "./Users";
+
+class UsersСontainer extends React.Component {
+
+    //Вызывается после рендеринга компонента
+    componentDidMount() {
+        //Подгружаем данные из api
+        if (this.props.users.length === 0) {
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+        }
+    }
+
+    //Метод для изменения страницы
+    onPageChanged = (page) => {
+        this.props.setCurrentPage(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount} pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage} onPageChanged={this.onPageChanged} users={this.props.users}
+                      addFriend={this.props.addFriend} delFriend={this.props.delFriend}/>
+    }
+}
 
 //Функция для передачи state
 let mapStateToProps = (state) => {
@@ -30,7 +60,7 @@ let mapDispatchToProps = (dispatch) => {
         setCurrentPage: (currentPage) => {
             dispatch(setCurrentPageAC(currentPage));
         },
-        //Функция для изменения страницы
+        //Функция для установки числа пользователей в переменную totalUsersCount страницы
         setTotalUsersCount: (totalUsersCount) => {
             dispatch(setTotalUsersCountAC(totalUsersCount));
         }
@@ -38,5 +68,5 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 //Создаем контейнерную компоненту с помощью react-redux
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersСontainer);
 export default UsersContainer;
